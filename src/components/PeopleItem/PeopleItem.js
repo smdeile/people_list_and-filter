f146aa09a6fcc37aee13416a57492e04929d5198
smdeile <smdeile@gmail.com>
2021-02-26 13:34:18 +0200

    Fix description of people sex

--- src/components/PeopleItem/PeopleItem.js
+++ src/components/PeopleItem/PeopleItem.js
@@ -1,10 +1,11 @@
 const PeopleItem = ({ people }) => {
   const { name, lastname, age, sex } = people;
+
   return (
     <li>
       <h2>{name + " " + lastname}</h2>
       <p>Возраст: {age}</p>
-      <p>Пол: {sex}</p>
+      <p>Пол: {sex === "m" ? "мужской" : "женский"}</p>
     </li>
   );
 };
